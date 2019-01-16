2d12fd840f6fcdbb80b6e2ea9f9114b8610672fd
RyanG91 <ryan.grosser91@gmail.com>
2019-01-16 10:29:27 +1000

    Added value and times challenge

--- 29_valueAndTimes.js
+++ 29_valueAndTimes.js
@@ -2,24 +2,22 @@
 Create a function which will take a value and multiply it a
 specific number of times. 
 
-valueAndTimes(value,times)
-
-If the times parameter is not a valid
-number the code should return null.
+If the times parameter is not a valid number the code should
+return null.
 
 Examples:
 valueAndTimes(9,2) == 9 * 2 == 18
 valueAndTimes("[alright]",3) == "[alright]" + "[alright]" + "[alright]" == "[alright][alright][alright]"
-valueAndTimes(5,"kanye west") == null
+valueAndTimes(5,["kanye west"]) == null
 
-If your feeling confident uncomment the beast mode tests below.
+If your feeling confident add more tests below.
 */
 
 const valueAndTimes = (value, times) => {
     // Your code here
 }
 
-const assert = require('assert')
+let assert = require('assert')
 
 describe('Multiply tests', () => {
     it('Should multiply the values and times when both are numbers', () => {
@@ -32,20 +30,6 @@ describe('Multiply tests', () => {
     it('Should return null when the times is not a number', () => {
         assert.equal(valueAndTimes(4,"[kanye west]"), null)
         assert.equal(valueAndTimes(2,"taylor swift"), null)
+        assert.equal(valueAndTimes(7, {a: "matthew mcconaughey"}), null)
     })
-    // ------------------------------------------------------------------------
-    //                           Beast Mode:
-    // ------------------------------------------------------------------------
-    // it('Beast mode: Should the times be in an array, multiply the array first then the value', () => {
-    //     assert.equal(valueAndTimes(9,[2,2]), 36)
-    //     assert.equal(valueAndTimes(4,[4,4]), 64)
-    //     assert.equal(valueAndTimes(4,[4,"apples"]), null)
-    // })
-    // it('Beast mode: should take the value of an object/s and multiply them and then multiply the value', () => {
-    //     assert.equal(valueAndTimes(2,{a: 3}), 6)
-    //     assert.equal(valueAndTimes(3,{a: 3, b: 2}), 18)
-    //     assert.equal(valueAndTimes(3,{a: 3, b: "matthew mcconaughey"}), null)
-    // })
 })
-
-
